bc60038bd1d8bc6b61dfadf59176df719584703a
Hamza amine amil <hamza.amineamil@gmail.com>
2025-07-27 21:38:15 +0100

    shopping manager changes

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,15 +9,15 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = int(input("Enter the item to add:"))
+        choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item to add: ").strip()
+            item = input("Enter item to add: ")
             shopping_list.append(item)
             print(f"'{item}' has been added to your shopping list.")
         
         elif choice == '2':
-            item = input("Enter item to remove: ").strip()
+            item = input("Enter item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' has been removed from your shopping list.")
