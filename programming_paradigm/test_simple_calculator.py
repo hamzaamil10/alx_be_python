8575d24a6adac959db65fbe2f529a7ed1ac0de77
Hamza amine amil <hamza.amineamil@gmail.com>
2025-08-04 00:12:05 +0100

    Calculator 1

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -24,7 +24,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(0, 100), 0)
 
-    def test_divide(self):
+    def test_division(self):
         """Test the division method, including divide by zero."""
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertIsNone(self.calc.divide(10, 0))  # Division by zero
